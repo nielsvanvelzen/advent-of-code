d30292be1f8b2e60e289a2df50b08c23b852a570
Niels van Velzen <git@ndat.nl>
2020-12-01 10:17:04 +0100

    Day 1 part 2

--- 2020-kotlin/src/main/kotlin/Day1.kt
+++ 2020-kotlin/src/main/kotlin/Day1.kt
@@ -10,4 +10,18 @@ object Day1 {
 			}
 		}
 	}
+
+	fun runPart2(input: String) {
+		val needle = 2020
+		val expenses = input.split("\n").map { it.toInt() }.sorted()
+		expenses.forEach { a ->
+			expenses.forEach { b ->
+				expenses.forEach { c ->
+					if (a + b + c == needle) {
+						println("$a + $b + $c = $needle | $a * $b * $c = ${a * b * c}")
+					}
+				}
+			}
+		}
+	}
 }

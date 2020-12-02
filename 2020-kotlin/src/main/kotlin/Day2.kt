e0da8e91d8bc842655d42fbcff7aa76f46d966c9
Niels van Velzen <git@ndat.nl>
2020-12-02 08:56:48 +0100

    Day 2 part 2

--- 2020-kotlin/src/main/kotlin/Day2.kt
+++ 2020-kotlin/src/main/kotlin/Day2.kt
@@ -6,8 +6,7 @@ object Day2 {
 	fun run(input: String) {
 		val policies = input.split("\n").map {
 			val values = regex.matchEntire(it)!!.groupValues
-			println(values)
-			val range = values[1].toInt() .. values[2].toInt()
+			val range = values[1].toInt()..values[2].toInt()
 			val letter = values[3].toCharArray().first()
 			val password = values[4]
 
@@ -16,6 +15,27 @@ object Day2 {
 
 		val validPasswords = policies.count { policy ->
 			policy.password.count { it == policy.letter } in policy.range
+		}
+
+		println("Valid passwords: $validPasswords")
+	}
+
+	fun runPart2(input: String) {
+		val policies = input.split("\n").map {
+			val values = regex.matchEntire(it)!!.groupValues
+			val range = values[1].toInt()..values[2].toInt()
+			val letter = values[3].toCharArray().first()
+			val password = values[4]
+
+			Policy(range, letter, password)
+		}
+
+		val validPasswords = policies.count { policy ->
+			val matchA = policy.password[policy.range.first - 1] == policy.letter
+			val matchB = policy.password[policy.range.last - 1] == policy.letter
+			println("$policy, $matchA, $matchB")
+
+			if (matchA != matchB) matchA || matchB else false
 		}
 
 		println("Valid passwords: $validPasswords")

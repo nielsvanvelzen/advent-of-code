6d532a0ed08767a9e348f0bed30edda2a00a20ba
Niels van Velzen <git@ndat.nl>
2021-12-01 09:37:26 +0100

    Day 01 - Part 02

--- 2021-kotlin/src/main/kotlin/Day01.kt
+++ 2021-kotlin/src/main/kotlin/Day01.kt
@@ -3,4 +3,10 @@ object Day01 {
 		val heights = input.lines().map(String::toInt)
 		return heights.indices.count { i -> i > 0 && heights[i] > heights[i - 1] }
 	}
+
+	fun runPart2(input: String): Int {
+		val heights = input.lines().map(String::toInt)
+		val windows = heights.indices.mapNotNull { i -> if (i > heights.size - 3) null else heights[i] + heights[i + 1] + heights[i + 2] }
+		return windows.indices.count { i -> i > 0 && windows[i] > windows[i - 1] }
+	}
 }
--- 2021-kotlin/src/test/kotlin/Day01Test.kt
+++ 2021-kotlin/src/test/kotlin/Day01Test.kt
@@ -2025,6 +2025,20 @@ class Day01Test {
 	@Test
 	fun day1_part1_input() {
 		val answer = Day01.runPart1(part1Input)
-		println("Answer for day1_part1_input is $answer")
+		println("Answer for day 1 part 1 is $answer")
+	}
+
+	private val part2SampleInput = part1SampleInput
+	private val part2Input = part1Input
+
+	@Test
+	fun day1_part2_sample() {
+		assertEquals(5, Day01.runPart2(part1SampleInput))
+	}
+
+	@Test
+	fun day1_part2_input() {
+		val answer = Day01.runPart2(part1Input)
+		println("Answer for day 1 part 2 is $answer")
 	}
 }

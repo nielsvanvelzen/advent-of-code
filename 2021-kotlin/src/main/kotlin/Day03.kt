6a7b00869e8c8dec880f44748ac5878f2ee9dd11
Niels van Velzen <git@ndat.nl>
2021-12-03 20:21:55 +0100

    Day 03 - Part 02

--- 2021-kotlin/src/main/kotlin/Day03.kt
+++ 2021-kotlin/src/main/kotlin/Day03.kt
@@ -24,4 +24,42 @@ object Day03 {
 
 		return gamma * epsilon
 	}
+
+	fun runPart2(input: String): Int {
+		val lines = input.lines()
+		val height = lines.size
+		val width = lines.first().length
+
+		val bits = Array(height) { row ->
+			BooleanArray(width) { column ->
+				lines[row][column] == '1'
+			}
+		}
+
+		val oxyRows = bits.copyOf().toMutableList()
+		val scrubberRows = bits.copyOf().toMutableList()
+		for (i in 0 until width) {
+			if (oxyRows.size > 1) {
+				val oxyCommon = oxyRows.count { it[i] }.toDouble() >= oxyRows.size / 2.0
+				oxyRows.removeIf { it[i] != oxyCommon }
+			}
+
+			if (scrubberRows.size > 1) {
+				val scrubberCommon = scrubberRows.count { it[i] }.toDouble() >= scrubberRows.size / 2.0
+				scrubberRows.removeIf { it[i] == scrubberCommon }
+			}
+		}
+
+		val oxygenRow = oxyRows.first()
+		val scrubberRow = scrubberRows.first()
+		var oxygenRating = 0
+		var scrubberRating = 0
+
+		for (index in 0 until width) {
+			if (oxygenRow[index]) oxygenRating += 2.0.pow(width - index - 1).toInt()
+			if (scrubberRow[index]) scrubberRating += 2.0.pow(width - index - 1).toInt()
+		}
+
+		return oxygenRating * scrubberRating
+	}
 }
--- 2021-kotlin/src/test/kotlin/Day03Test.kt
+++ 2021-kotlin/src/test/kotlin/Day03Test.kt
@@ -15,4 +15,18 @@ class Day03Test {
 		val answer = Day03.runPart1(part1Input)
 		println("Answer for day 3 part 1 is $answer")
 	}
+
+	private val part2SampleInput = part1SampleInput
+	private val part2Input = part1Input
+
+	@Test
+	fun day3_part2_sample() {
+		assertEquals(230, Day03.runPart2(part1SampleInput))
+	}
+
+	@Test
+	fun day3_part2_input() {
+		val answer = Day03.runPart2(part1Input)
+		println("Answer for day 3 part 2 is $answer")
+	}
 }

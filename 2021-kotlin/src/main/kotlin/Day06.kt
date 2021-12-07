75e32b7ac1dc88b35cc8260b34a892ecc78f9e95
Niels van Velzen <git@ndat.nl>
2021-12-07 09:04:43 +0100

    Day 06 - Part 02

--- 2021-kotlin/src/main/kotlin/Day06.kt
+++ 2021-kotlin/src/main/kotlin/Day06.kt
@@ -1,19 +1,36 @@
 object Day06 {
-	fun runPart1(input: String): Int {
-		val fish = input.split(",").map(String::toInt).toMutableList()
-
-		repeat(80) {
-			for (index in fish.indices) {
-				when (fish[index]) {
-					0 -> {
-						fish[index] = 6
-						fish.add(8)
-					}
-					else -> fish[index]--
-				}
-			}
+	fun run(input: String, days: Int): Long {
+		val ages = mutableMapOf(
+			0 to 0L,
+			1 to 0L,
+			2 to 0L,
+			3 to 0L,
+			4 to 0L,
+			5 to 0L,
+			6 to 0L,
+			7 to 0L,
+			8 to 0L,
+		)
+		input.split(",").map(String::toInt).forEach {
+			ages[it] = ages[it]!! + 1L
 		}
 
-		return fish.size
+		repeat(days) {
+			val z = ages[0]!!
+			ages[0] = ages[1]!!
+			ages[1] = ages[2]!!
+			ages[2] = ages[3]!!
+			ages[3] = ages[4]!!
+			ages[4] = ages[5]!!
+			ages[5] = ages[6]!!
+			ages[6] = ages[7]!! + z
+			ages[7] = ages[8]!!
+			ages[8] = z
+		}
+
+		return ages.values.sum()
 	}
+
+	fun runPart1(input: String): Long = run(input, 80)
+	fun runPart2(input: String): Long = run(input, 256)
 }

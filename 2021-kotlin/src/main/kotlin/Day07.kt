59cbd592511b15215e9db180bbe94e2811672b96
Niels van Velzen <git@ndat.nl>
2021-12-08 17:04:46 +0100

    Day 07 - Part 02

--- 2021-kotlin/src/main/kotlin/Day07.kt
+++ 2021-kotlin/src/main/kotlin/Day07.kt
@@ -11,4 +11,18 @@ object Day07 {
 			positions.sumOf { abs(it - target) }
 		}
 	}
+
+	fun runPart2(input: String): Int {
+		val positions = input.split(",").map(String::toInt)
+
+		val min = positions.minOrNull() ?: 0
+		val max = positions.maxOrNull() ?: 1
+
+		return (min..max).minOf { target ->
+			positions.sumOf {
+				val diff = abs(it - target)
+				diff * (diff + 1) / 2
+			}
+		}
+	}
 }
--- 2021-kotlin/src/test/kotlin/Day07Test.kt
+++ 2021-kotlin/src/test/kotlin/Day07Test.kt
@@ -15,4 +15,15 @@ class Day07Test {
 		val answer = Day07.runPart1(input)
 		println("Answer for day 7 part 1 is $answer")
 	}
+
+	@Test
+	fun day7_part2_sample() {
+		assertEquals(168, Day07.runPart2(sample))
+	}
+
+	@Test
+	fun day7_part2_input() {
+		val answer = Day07.runPart2(input)
+		println("Answer for day 7 part 2 is $answer")
+	}
 }
